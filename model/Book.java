9bec3bd4a7f6c3688063eb2848aaf2b066a63cc1
sheng <1591330057@qq.com>
2017-09-24 01:11:24 +0800

    Create Book.java

---
+++ model/Book.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    public Book(){
+    }
+    public Book(int _id,String _name,String _author){
+    	id=_id;
+    	name=_name;
+    	author=_author;
+    }
+    public void setId(int _id){
+    	id=_id;
+    }
+    public void setName(String _name){
+    	name=_name;
+    }
+    public void setAuthor(String _author){
+    	author=_author;
+    }
+    public int getId(){
+    	return id;
+    }
+    public String getName(){
+    	return name;
+    }
+    public String getAuthor(){
+    	return author;
+    }
+}
